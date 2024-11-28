083447a2b95623b0ca96f1c6c7f60406338277c3
chan-764 <chandanuikey19@gmail.com>
2024-11-28 14:50:36 +0530

    Update and rename init.sql to init.js

---
+++ db/init.js
@@ -0,0 +1,24 @@
+
+// Access the 'quotes' collection (MongoDB will create this if it doesn't exist)
+db.quotes.insertMany([
+    {
+        quote: "Be the change you want to see in the world.",
+        author: "Ghandi"
+    },
+    {
+        quote: "Master patterns in life, and you'll never suffer is the secret",
+        author: "someone"
+    }
+]);
+
+
+
+
+
+
+
+
+
+
+
+

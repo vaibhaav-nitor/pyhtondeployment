1bb1b2eca5fae9009e963b60089d8d3d916d0d12
chan-764 <chandanuikey19@gmail.com>
2024-12-05 22:57:47 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -147,7 +147,7 @@ def index():
 if __name__ == "__main__":
     # Use the PORT environment variable, defaulting to 5002 for local development
     port = int(os.environ.get("PORT", 5002))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5002)
 
 
 

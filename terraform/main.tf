92eb23035d01599f47c609fdfb6e0f4636b8d384
chan-764 <chandanuikey19@gmail.com>
2025-01-14 10:58:31 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,9 @@ resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
+  tags = {
+    Name = "eks-vpc"
+  }
 }
 
 # Subnet Configuration
@@ -15,37 +18,105 @@ resource "aws_subnet" "az1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-west-2a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "eks-subnet-az1"
+  }
 }
 
 resource "aws_subnet" "az2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-west-2b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "eks-subnet-az2"
+  }
 }
 
 resource "aws_subnet" "az3" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.3.0/24"
   availability_zone = "us-west-2c"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "eks-subnet-az3"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "eks-internet-gateway"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "eks-route-table"
+  }
+}
+
+# Associate Route Table with Subnets
+resource "aws_route_table_association" "az1" {
+  subnet_id      = aws_subnet.az1.id
+  route_table_id = aws_route_table.main.id
+}
+
+resource "aws_route_table_association" "az2" {
+  subnet_id      = aws_subnet.az2.id
+  route_table_id = aws_route_table.main.id
+}
+
+resource "aws_route_table_association" "az3" {
+  subnet_id      = aws_subnet.az3.id
+  route_table_id = aws_route_table.main.id
 }
 
 # Security Group for Worker Nodes
 resource "aws_security_group" "worker_nodes" {
   name        = "eks-worker-nodes-sg"
-  description = "Allow all traffic within the VPC for EKS worker nodes"
+  description = "Allow traffic for EKS worker nodes"
   vpc_id      = aws_vpc.main.id
 
+  # Allow all traffic within the VPC
   ingress {
     from_port   = 0
     to_port     = 65535
-    protocol    = "tcp"
+    protocol    = "-1"
     cidr_blocks = ["10.0.0.0/16"]
   }
 
+  # Allow HTTPS traffic to the cluster
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow traffic for Kubernetes API (kubelet)
+  ingress {
+    from_port   = 10250
+    to_port     = 10250
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic
   egress {
     from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -57,15 +128,12 @@ resource "aws_iam_role" "eks_cluster_role" {
     Version = "2012-10-17"
     Statement = [
       {
-        Action = [
-          "sts:AssumeRole",
-          "sts:TagSession"
-        ]
+        Action = "sts:AssumeRole"
         Effect = "Allow"
         Principal = {
           Service = "eks.amazonaws.com"
         }
-      },
+      }
     ]
   })
 }
@@ -103,6 +171,11 @@ resource "aws_iam_role_policy_attachment" "worker_nodes_ecr_policy" {
   role       = aws_iam_role.worker_nodes_role.name
 }
 
+resource "aws_iam_role_policy_attachment" "worker_nodes_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.worker_nodes_role.name
+}
+
 # EKS Cluster Configuration
 resource "aws_eks_cluster" "main" {
   name     = "three-tier-cluster-2"
@@ -114,6 +187,7 @@ resource "aws_eks_cluster" "main" {
       aws_subnet.az2.id,
       aws_subnet.az3.id
     ]
+    security_group_ids = [aws_security_group.worker_nodes.id]
   }
 }
 
@@ -130,35 +204,4 @@ resource "aws_eks_node_group" "main" {
     max_size     = 2
     min_size     = 2
   }
-}
-
-# Data Resource for Cluster Authentication
-data "aws_eks_cluster_auth" "main" {
-  name = aws_eks_cluster.main.name
-}
-
-# Output the cluster kubeconfig
-output "kubeconfig" {
-  value = <<EOT
-apiVersion: v1
-clusters:
-- cluster:
-    server: ${aws_eks_cluster.main.endpoint}
-    certificate-authority-data: ${aws_eks_cluster.main.certificate_authority[0].data}
-  name: ${aws_eks_cluster.main.name}
-contexts:
-- context:
-    cluster: ${aws_eks_cluster.main.name}
-    user: eks-admin
-  name: ${aws_eks_cluster.main.name}
-current-context: ${aws_eks_cluster.main.name}
-kind: Config
-users:
-- name: eks-admin
-  users:
-- name: eks-admin
-  user:
-    token: ${data.aws_eks_cluster_auth.main.token}
-EOT
-  sensitive = true  # Mark output as sensitive to avoid logging exposure
 }
